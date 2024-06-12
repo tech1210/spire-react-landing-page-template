91e4698fbe350ad49e98526e050544c78e6a8c4d
tech1210 <techinfo1210@gmail.com>
2024-06-12 12:06:50 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  List,
+  ListItem,
+  Typography,
+  styled,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import DrawerItem from "./DrawerItem";
+import { Link } from "react-router-dom";
+import LogoWhite from "../assets/logo1.png";
+
+// personalizacao
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const ListMenu = styled(List)(({ theme }) => ({
+  display: "none",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+//rotas
+const itemList = [
+  {
+    text: "Home",
+    to: "/",
+  },
+  {
+    text: "About",
+    to: "/about",
+  },
+  {
+    text: "Contact",
+    to: "/contact",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <AppBar
+      component="nav"
+      position="sticky"
+      sx={{
+        backgroundColor: "#3e3c61",
+      }}
+      elevation={0}
+    >
+      <StyledToolbar>
+        <Typography variant="h6" component="h2">
+          <Link to="/">
+            <img
+              src={LogoWhite}
+              style={{ height: "50px", color: "#6842FF" }}
+              alt="logo"
+            />
+          </Link>
+        </Typography>
+        <Box sx={{ display: { xs: "block", sm: "none" } }}>
+          <DrawerItem />
+        </Box>
+        <ListMenu>
+          {itemList.map((item) => {
+            const { text } = item;
+            return (
+              <ListItem key={text}>
+                <ListItemButton
+                  component={Link}
+                  to={item.to}
+                  sx={{
+                    color: "#fff",
+                    "&:hover": {
+                      backgroundColor: "transparent",
+                      color: "#ccc",
+                    },
+                  }}
+                >
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </ListMenu>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
